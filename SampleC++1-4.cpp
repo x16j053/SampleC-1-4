3a7b25f0ac212babeaeba781c7d16e2e46a79dde
WIN_NT\x16j053 <x16j053@chiba-fjb.ac.jp>
2017-05-23 12:31:10 +0900

    文字列

---
+++ SampleC++1-4.cpp
@@ -0,0 +1,19 @@
+// SampleC++1-4.cpp : コンソール アプリケーションのエントリ ポイントを定義します。
+//
+
+#include "stdafx.h"
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+	string s, t;
+	t = "入力された文字は、";
+	cout << "文字列を入力：";
+	cin >> s;
+	cout << t + s << "です。" << endl;
+    return 0;
+}
+
